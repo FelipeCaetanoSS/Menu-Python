265eb11a0c39fab3ee42e356314ee85efe601d52
Felipe Caetano <126731108+FelipeCaetanoSS@users.noreply.github.com>
2025-06-26 08:58:41 -0300

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,39 +1,57 @@
-lista_itens = ["Prato 1","Prato 2", "Prato 3", "Prato 4", "Prato 5","Prato 6","Prato 7","Prato 8","Prato 9", "Prato 10", "Prato 11","Pedidos","Finaliizar compra"]
-lista_preco = [29.99, 29.99, 34.99, 19.99, 23.99, 19.99, 26.99, 32.99, 40.99, 22.99, 27.99,0.0,0.0]
+lista_itens = ["Prato 1","Prato 2", "Prato 3", "Prato 4", "Prato 5","Prato 6","Prato 7","Prato 8","Prato 9", "Prato 10", "Prato 11"]
+lista_preco = [29.99, 29.99, 34.99, 19.99, 23.99, 19.99, 26.99, 32.99, 40.99, 22.99, 27.99] 
 pedidos = []
-valor_total = 0
+valor_total = 0.0
 item = -1
 
-print("Seja bem - vindo!!")
-print("----------------------------------------------------------------\n")
+print("    Seja bem - vindo!!")
+print("----------------------------\n")
 
-while item != 13:
+while item != 0:
+    print("\tCARDÁPIO\n")
     for i in range(len(lista_itens)):
-        print(i, lista_itens[i], lista_preco[i])
+      print(f"{i + 1}. {lista_itens[i]} - R${lista_preco[i]}")
+    
+    print("\n0 para finalizar a compra!")
+    print("12 para exibir os pedidos!")
+    print("\n----------------------------\n")
 
-    escolha = input("\n Adicionar pedido: ")
-    item = int(escolha)
+    try:
+        escolha = int(input("Adicionar pedido: "))
+        item = escolha 
+    except ValueError:
+        print("Entrada inválida.")
+        print("Por favor, digite um número.\n")
+        print("----------------------------\n")
+        continue
 
-    if item < 12:
-        pedidos.append(item)
-        valor_total += lista_preco[item]
-        print("Item adicionado ao pedidos! \n")
-        print("----------------------------------------------------------------\n")
+    if item > 0 and item < 12:
+        pedidos.append(item - 1)
+        valor_total += lista_preco[item - 1]
+        print(f"\n{lista_itens[item - 1]}")
+        print("Item adicionado aos pedidos! \n")
+        print("----------------------------\n")
 
     elif item == 12:
-        print("PEDIDOS")
+        print("\nPEDIDOS:")
         for i in range(len(pedidos)):
-            print("Item:", lista_itens[i])
-            print("VALOR TOTAL: R$ \n", valor_total)
-            print("----------------------------------------------------------------\n")
-            
+          print(f"Item: {lista_itens[pedidos[i]]}.\n") 
+        print("Valor total: R$", valor_total)
+        print("\n----------------------------\n")
+
+    elif item == 0:
+        print("Finalizando compra...")
+        print("\n----------------------------\n")
+        break 
+
     else:
-        print("Erro, tente novante! \n")
-        print("----------------------------------------------------------------\n")
+        print("Erro!")
+        print("Tente números que estão no cardápio\n")
+        print("----------------------------\n")
 
 print("Compra finalizada.\n")
-print("Obrigado pela preferência!\n\n")
+print("Obrigado pela preferência!\n")
 print("PEDIDOS: \n")
-for i in range(len(pedidos)):
-    print(lista_itens[i])
+for item_index in pedidos: 
+    print(lista_itens[item_index])
 print(f"Valor total: R${valor_total}")
